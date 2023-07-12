aeb3395e5bbe970cb34c08b01c94cb495a7d520d
Aynur Shamsullin <lilmoon99@ya.ru>
2023-07-12 21:25:12 +0400

    lesson 1 commit

---
+++ Lectures/Урок 1. Знакомство с языком программирования Python/main.py
@@ -0,0 +1,93 @@
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# print(f"{a} - {b} - {c}") ctrl+k ctrl+c to comment// ctrl+k ctrl+u to uncomment
+# print("{} - {} - {}".format(a,b,c))
+
+# print('Enter first number: ')
+# a = int(input())
+# b = int(input('Enter second number: '))
+# print(a,'+',b,'=',a+b)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# a = 5.123512
+# b = 6.1235
+# print(round((a*b),4))
+
+# iter = 2
+# iter += 3 #iter = iter + 3
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 == 2
+# print(a)
+# a = 1 != 2
+# print(a)
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+# a = 1 < 3 < 5 < 10
+# print(a)
+
+# username = input('Enter name: ')
+# if username == 'Маша':
+#     print('Ура, это же Маша')
+# elif username == 'Марина':
+#     print('Я так ждала Вас, Марина')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ')
+# else:
+#     print('Привет,',username)
+
+
+# n = 423
+# summa = 0
+# while n > 0:
+#     x = n % 10
+#     summa = summa + x
+#     n = n// 10
+# print(summa)
+
+# i = 0
+# while i < 5:
+#     if i == 3 :
+#         break
+#     i= i + 1
+# else:
+#     print('Пожалуй')
+#     print('хватит')
+# print(i)
+
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print(i)
+#     elif i > n // 2:
+#         print(n)
+#         flag = False
+#     i += 1
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += '*'
+#     print(line)
+
