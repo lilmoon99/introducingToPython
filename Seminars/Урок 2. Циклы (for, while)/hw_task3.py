74d8cd27670d56ce830523ce5849f5c84aea8f36
Aynur Shamsullin <lilmoon99@ya.ru>
2023-07-19 20:17:30 +0400

    hw task 3 solved

--- Seminars/Урок 2. Циклы (for, while)/hw_task3.py
+++ Seminars/Урок 2. Циклы (for, while)/hw_task3.py
@@ -1,1 +1,21 @@
 # Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+import math
+
+N = int(input("Введите число N: "))
+base = 2
+
+def calculate(N):
+    list = []
+    for i in range(N):
+        list.append(pow(base,i))
+    return list
+        
+def print_list(list):
+    print(f"Степени числа {base} до ^{N}")
+    for i in list:
+        print(i)
+        
+        
+pow2 = calculate(N)
+print_list(pow2)
