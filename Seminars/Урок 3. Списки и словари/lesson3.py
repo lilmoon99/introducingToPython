f43acb76aab8b24e2ad3d1484b2b3da93d937c4b
Aynur Shamsullin <lilmoon99@ya.ru>
2023-07-21 21:20:18 +0400

    lesson 3 prepare

---
+++ Seminars/Урок 3. Списки и словари/lesson3.py
@@ -0,0 +1,42 @@
+sp = []
+sp.append(5)
+sp.extend([7,8,True])
+sp.insert(0,5.7)
+sp = sp + [1,2]
+# print(sp[3::])
+# print(sp[2:5])
+# print(sp[-5])
+# print(sp)
+
+# a = sp.pop()
+# print(a)
+# print(sp)
+
+# sp.remove(True)
+# del sp[0]
+# sp.clear() #Удаляет всё
+# print(sp)
+
+# t = (4,8,9)
+# print(t)
+# print(t[0])
+
+# d = {}
+# d["дядя Ваня"] = "+712398123"
+# d["дядя Вова"] = "8908712312"
+# del d["дядя Вова"]
+
+# print(d)
+
+# for key,value in d.items():
+#     print(key,value)
+    
+# print(d.keys())
+# print(d.values())
+
+# s = set()
+
+# s.add(5)
+# s.add(7)
+# print(s)
+# print(5 in s)
