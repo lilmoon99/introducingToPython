2b41e3f2f8f8b71067306f32553d22eb1cc3e7c7
Aynur Shamsullin <lilmoon99@ya.ru>
2023-07-25 22:10:30 +0400

    task 1 and 2 solved

--- Seminars/Урок 4. Словари, множества и профилирование/task1.py
+++ Seminars/Урок 4. Словари, множества и профилирование/task1.py
@@ -5,3 +5,20 @@
 # Input: a a a b c a a d c d d
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 # Для решения данной задачи используйте функцию .split()
+
+k = input("Введите символы через пробел :").split()
+print(k)
+d = {}
+result = ""
+for i in k:
+    if i in d:
+        d[i] += 1
+    elif i not in d:
+        d[i] = 0
+    if d[i] == 0:
+        result+=f"{i} "
+    else:
+        result+=f" {i}_{d[i]} "
+    
+print(d)
+print(result)
