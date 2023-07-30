0c5378d0ce50dc8bf13dde541886c16f44c6294e
Aynur Shamsullin <lilmoon99@ya.ru>
2023-07-30 12:27:56 +0400

    hw task 1 solvd

--- Seminars/Урок 4. Словари, множества и профилирование/hw_task1.py
+++ Seminars/Урок 4. Словари, множества и профилирование/hw_task1.py
@@ -7,7 +7,8 @@ from random import *
 def generator(length:int) -> list:
     temp_list = []
     for i in range(length):
-        temp_list.append(randint(1,10))
+        # temp_list.append(randint(1,10)) # filling list with random numbers
+        temp_list.append(int(input(f"Введите {i+1} элемент набора: "))) # filling list one by one via printing
     return temp_list
 
 def compare_lists(list1:list,list2:list) -> set:
@@ -19,16 +20,13 @@ def compare_lists(list1:list,list2:list) -> set:
     temp_set = sorted(temp_set)
     return temp_set
 
-def user_comparator(numbers,set):
-    for i in range(numbers):
-        if numbers[i] in set:
-            print(numbers[i])
-list_1 = generator(5)
-print(list_1)
-list_2 = generator(10)
-print(list_2)
+list_1 = generator(int(input("Введите количество элементов первого набора чисел: ")))
+list_2 = generator(int(input("Введите количество элементов второго набора чисел: ")))
+print(f"Первый набор чисел: {list_1}")
+print(f"Второй набор чисел: {list_2}")
 repeats = compare_lists(list_1,list_2)
-print(repeats)
+print(f"Повторяющиеся элементы из двух списков: {repeats}")
+
 
 
 
